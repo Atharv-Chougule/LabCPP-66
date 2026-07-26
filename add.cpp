3aada9788078bfe7f10fa4ab19050e413efa80a2
Atharv-Chougule <atharvv0601@gmail.com>
2026-07-27 01:35:00 +0530

    Update add.cpp

--- add.cpp
+++ add.cpp
@@ -1,1 +1,15 @@
+#include<iostream>
+using namespace std ; 
+
+int main()
+{
+    int num1 , num2 ,sum ;
+    
+    cout<<"Enter num : ";
+    cin >> num1 >> num2 ;
+      sum = num1 + num2 ;
+    cout<<"sum is : "<< sum ;
+    
+    return 0 ;
+}
 
