1fcf2466b58941ddac930b2be46ee38c46ba866d
Atharv-Chougule <atharvv0601@gmail.com>
2026-08-10 04:39:03 +0000

    Lab Programs

--- avg.cpp
+++ avg.cpp
@@ -1,1 +1,14 @@
+#include<iostream>
+using namespace std ;
 
+int main ()
+{
+    int num1 , num2 , sum , avg ;
+    cout<<"Enter Two  Number : ";
+    cin >> num1 ;
+    cin >> num2 ;
+    sum = num1 + num2 ;
+    avg = sum / 2 ;
+
+    cout<<"Sum of two number : "<<sum << "Avrage : "<<avg ;
+}
